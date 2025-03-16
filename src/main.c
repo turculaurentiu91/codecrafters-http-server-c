a10a7494381f6f2008fb5d8c24df2454de4278de
Laurentiu Turcu <turculaurentiu91@gmail.com>
2025-03-16 09:27:55 +0100

    added files endpoint

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <errno.h>
+#include <fcntl.h>
+#include <getopt.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <stddef.h>
@@ -10,18 +12,18 @@
 
 #include "strings.h"
 
-void process_request(unsigned int client_fd) {
+void process_request(unsigned int client_fd, char *directory_str) {
 
   char msg_200[19] = "HTTP/1.1 200 OK\r\n\r\n";
   char msg_404[31] = "HTTP/1.1 404 Not Found\r\n\r\n";
   char msg_400[33] = "HTTP/1.1 400 Bad Request\r\n\r\n";
+  char msg_500[34] = "HTTP/1.1 500 Server Error\r\n\r\n";
 
   string_t request_str = string_new(32 * 1024);
   size_t read_data = read(client_fd, request_str.head, request_str.capacity);
   if (read_data == -1) {
     printf("Could not read from the client soket");
-    close(client_fd);
-    return;
+    goto final;
   }
 
   request_str.length = read_data;
@@ -34,8 +36,7 @@ void process_request(unsigned int client_fd) {
     written = write(client_fd, &msg_400, 33);
     if (written == -1) {
       printf("Could not write to the client socket");
-      close(client_fd);
-      return;
+      goto final;
     }
   }
 
@@ -75,27 +76,90 @@ void process_request(unsigned int client_fd) {
                  echo_val.length, (int)echo_val.length, echo_val.head);
 
     written = write(client_fd, res, res_size);
+  } else if (path.length > 7 && string_slice_starts_with(&path, "/files/")) {
+    string_slice_t file_name = string_slice_slice(&path, 7, path.length - 6);
+    size_t path_length = strlen(directory_str) + file_name.length + 1;
+    char path[path_length];
+    snprintf(path, path_length, "%s%.*s", directory_str, (int)file_name.length,
+             file_name.head);
+
+    printf("file path is: %s\n", path);
+    int file_fd = open(path, O_RDONLY);
+    if (file_fd == -1) {
+      if (errno == ENOENT) {
+        written = write(client_fd, &msg_404, 31);
+        goto final;
+      }
+
+      written = write(client_fd, &msg_500, 34);
+      goto final;
+    }
+
+    string_t buffer = string_new(1024 + 32);
+    read_data = read(file_fd, buffer.head, buffer.capacity);
+    buffer.length = read_data;
+    if (read_data == -1) {
+      printf("Could not read from the file: %s\n", path);
+      written = write(client_fd, &msg_500, 34);
+    } else {
+
+      char res[1024];
+      size_t res_size =
+          snprintf(res, sizeof(res),
+                   "HTTP/1.1 200 OK\r\nContent-Type: "
+                   "application/octet-stream\r\nContent-Length: %ld\r\n\r\n",
+                   buffer.length);
+
+      written = write(client_fd, &res, res_size);
+      written += write(client_fd, buffer.head, buffer.length);
+    }
+    close(file_fd);
   } else {
     written = write(client_fd, &msg_404, 31);
   }
 
   if (written == -1) {
     printf("Could not write to the client socket");
-    close(client_fd);
-    return;
   }
 
+final:
   close(client_fd);
+  string_free(&request_str);
+  return;
 }
 
-int main() {
+int main(int argc, char **argv) {
   // Disable output buffering
   setbuf(stdout, NULL);
   setbuf(stderr, NULL);
 
+  char *directory_str = NULL;
+  static struct option long_options[] = {
+      /*   NAME       ARGUMENT           FLAG  SHORTNAME */
+      {"directory", required_argument, NULL, 'd'},
+  };
+
+  int option_index = 0;
+  char c;
+  while ((c = getopt_long(argc, argv, "d:", long_options, &option_index)) !=
+         -1) {
+    switch (c) {
+    case 'd':
+      directory_str = optarg;
+      break;
+    default:
+      break;
+    }
+  }
+
+  if (directory_str == NULL) {
+    directory_str = "/tmp/";
+  }
+
   // You can use print statements as follows for debugging, they'll be visible
   // when running tests.
   printf("Logs from your program will appear here!\n");
+  printf("directory is: %s\n", directory_str);
 
   int server_fd;
   struct sockaddr_in client_addr;
@@ -153,7 +217,7 @@ int main() {
 
     case 0:
       close(server_fd);
-      process_request(client_fd);
+      process_request(client_fd, directory_str);
       _exit(EXIT_SUCCESS);
 
     default:
